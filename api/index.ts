1af2d037bad6e5899e24a8a040b4141b97d75e93
kmar <minedreams@qq.com>
2023-08-27 18:44:31 +0800

    调整测试字符串

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,12 @@
 import {VercelRequest, VercelResponse} from "@vercel/node"
-import {connectRedis} from './utils/RedisOperator'
 import {connectDatabase, initRequest} from './utils/utils'
 
 // noinspection JSUnusedGlobalSymbols
 export default async function(request: VercelRequest, response: VercelResponse) {
     const time = Date.now()
     const db = await connectDatabase()
-    await db.collection('c-test').insertOne({a: 'a'})
-    await db.collection('c-test').deleteOne({a: 'a'})
+    await db.collection('c-test').insertOne({a: 'test content'})
+    await db.collection('c-test').deleteOne({a: {$exists: true}})
     const endTime = Date.now()
     return response.status(200).send(endTime - time)
     // const checkResult = await initRequest(request, response, {allows: 'china'}, 'GET')
