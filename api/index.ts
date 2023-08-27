498b6668a553fd93e3075cde84a31b7ac252bd1b
kmar <minedreams@qq.com>
2023-08-27 18:42:20 +0800

    测试 MongoDB 性能

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,13 @@
 import {VercelRequest, VercelResponse} from "@vercel/node"
 import {connectRedis} from './utils/RedisOperator'
-import {initRequest} from './utils/utils'
+import {connectDatabase, initRequest} from './utils/utils'
 
 // noinspection JSUnusedGlobalSymbols
 export default async function(request: VercelRequest, response: VercelResponse) {
     const time = Date.now()
-    await connectRedis().set('test', 'test content')
-    await connectRedis().del('test')
+    const db = await connectDatabase()
+    await db.collection('c-test').insertOne({a: 'a'})
+    await db.collection('c-test').deleteOne({a: 'a'})
     const endTime = Date.now()
     return response.status(200).send(endTime - time)
     // const checkResult = await initRequest(request, response, {allows: 'china'}, 'GET')
