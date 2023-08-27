48cc2b84672ec5f3f9cc60a50e030bee8fc1db81
kmar <minedreams@qq.com>
2023-08-27 18:21:43 +0800

    添加性能测试

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,17 @@
 import {VercelRequest, VercelResponse} from "@vercel/node"
+import {connectRedis} from './utils/RedisOperator'
 import {initRequest} from './utils/utils'
 
 // noinspection JSUnusedGlobalSymbols
 export default async function(request: VercelRequest, response: VercelResponse) {
-    const checkResult = await initRequest(request, response, {allows: 'china'}, 'GET')
-    if (!checkResult) return
-    const {ip, location, count} = checkResult
-    console.log(`${ip}: ${location}`)
-    response.status(200).json({location, count})
+    const time = Date.now()
+    await connectRedis().set('test', 'test content')
+    await connectRedis().del('test')
+    const endTime = Date.now()
+    return response.status(200).send(endTime - time)
+    // const checkResult = await initRequest(request, response, {allows: 'china'}, 'GET')
+    // if (!checkResult) return
+    // const {ip, location, count} = checkResult
+    // console.log(`${ip}: ${location}`)
+    // response.status(200).json({location, count})
 }
