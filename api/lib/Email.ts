abba241ecc26ae7a799e462e434414cac62b606a
kmar <minedreams@qq.com>
2023-08-29 17:52:56 +0800

    支持发送任意类型的邮件

--- api/lib/Email.ts
+++ api/lib/Email.ts
@@ -6,21 +6,24 @@ import {EmailContentInfo, loadConfig} from './ConfigLoader'
 let transporter: Transporter<SMTPTransport.SentMessageInfo>
 
 /**
- * 发送邮件到指定邮箱
+ * 发送评论通知邮件到指定邮箱
  * @param to 指定的邮箱
  * @param info 附加信息
  */
-export async function sendTo(to: string, info: EmailContentInfo) {
+export async function sendReplyTo(to: string, info: EmailContentInfo) {
+    const config = loadConfig().email!
+    return sendTo(to, config.title, config.text?.(info), config.html?.(info), config.amp?.(info))
+}
+
+/** 发送任意邮件 */
+export async function sendTo(to: string, subject: string, text?: string, html?: string, amp?: string) {
     const transporter = initTransporter()
     if (!transporter) return false
     const config = loadConfig().email!
     return transporter.sendMail({
         from: `${config.name} <${config.fromEmail ?? config.user}>`,
-        to,
-        subject: config.title,
-        text: config.text?.(info),
-        html: config.html?.(info),
-        amp: config.amp?.(info)
+        to, subject,
+        text, html, amp
     })
 }
 
