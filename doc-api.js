f4c1c36a44fdbf7556b8a1253247d9b54ffcd761
kmar <minedreams@qq.com>
2023-09-06 22:05:27 +0800

    接口不再返回 Response

--- doc-api.js
+++ doc-api.js
@@ -6,21 +6,31 @@
  * @constructor
  */
 function Kments(api, nameGetter, emailGetter, linkGetter) {
-    const postHelper = (content, extraBody) => fetch(`${api}post-comment/`, {
-        method: 'POST',
-        body: JSON.stringify(Object.assign({
+    const fetchHelper = (method, type, params) => {
+        const promise = method === 'GET' ?
+            fetch(`${api}${type}/?${params}`) :
+            fetch(`${api}${type}/`, {method, body: params ? JSON.stringify(params) : undefined})
+        return promise.then(response => {
+            if (!response.ok) throw response
+            return response.json()
+        }).then(json => {
+            if (json.status !== 200) throw json
+            return json
+        })
+    }
+    const postHelper = (content, extraBody) =>
+        fetchHelper('POST', 'post-comment', Object.assign({
             page: location.pathname,
             name: nameGetter(),
             email: emailGetter(),
             link: linkGetter(),
             content,
             pageTitle: document.title
-        }, extraBody))
-    })
+        }, extraBody)).then(json => json.data)
     /**
      * 发布一个评论
      * @param content {string} 评论内容（HTML 格式）
-     * @return {Promise<Response>}
+     * @return {Promise<{id: string, location: string}>}
      */
     this.post = (content) => postHelper(content, {})
     /**
@@ -28,7 +38,7 @@ function Kments(api, nameGetter, emailGetter, linkGetter) {
      * @param root 评论所在的主楼评论的 ID
      * @param content 评论内容（HTML 格式）
      * @param ats 要 AT 的子评论的 ID
-     * @return {Promise<Response>}
+     * @return {Promise<{id: string, location: string}>}
      */
     this.reply = (root, content, ...ats) => postHelper(content, {
         reply: root,
@@ -41,7 +51,13 @@ function Kments(api, nameGetter, emailGetter, linkGetter) {
      * @param postmaster 父评论 ID
      * @param pathname 评论所在页面的 pathname
      * @param truth 是否显示隐藏的评论[0-不显示，1-显示，2-只显示隐藏评论]
-     * @return {Promise<Response>}
+     * @return {Promise<{
+     *      id: string,
+     *      name: string, email: string, link?: string,
+     *      location: string,
+     *      content: string,
+     *      subCount: number, hide?: boolean
+     * }[]>}
      */
     this.get = (start = 0, limit = 10, postmaster = '', pathname = location.pathname, truth = 0) => {
         const params = [`page=${encodeURIComponent(pathname)}`]
@@ -49,63 +65,57 @@ function Kments(api, nameGetter, emailGetter, linkGetter) {
         if (limit !== 10) params.push(`len=${limit}`)
         if (postmaster) params.push(`id=${postmaster}`)
         if (truth) params.push(`truth=${truth}`)
-        return fetch(`${api}get-comments/?${params.join('&')}`)
+        return fetchHelper('GET', 'get-comments', params.join('&'))
+            .then(json => json.data)
     }
     /**
      * 获取最新的 N 条评论
      * @param limit 最大数量限制（最大为 10）
-     * @return {Promise<Response>}
+     * @return {Promise<{
+     *      id: string,
+     *      name: string, email: string, link?: string,
+     *      location: string,
+     *      content: string,
+     *      subCount: number
+     * }[]>}
      */
-    this.recently = (limit = 5) => fetch(`${api}get-recent-comments/?limit=${limit}`)
+    this.recently = (limit = 5) =>
+        fetchHelper('GET', 'get-recent-comments', `limit=${limit}`)
+            .then(json => json.data)
     /**
      * 修改指定评论的内容
      * @param id 要修改的评论的 ID
      * @param content 修改后的内容（HTML 格式）
      * @param page 评论所在页面的 pathname
-     * @return {Promise<Response>}
+     * @return {Promise<any>}
      */
-    this.update = (id, content, page = location.pathname) => fetch(`${api}update-comment/`, {
-        method: 'POST',
-        body: JSON.stringify({id, content, page})
-    })
+    this.update = (id, content, page = location.pathname) =>
+        fetchHelper('PUT', 'update-comments', {id, content, page})
     /**
      * 管理员身份认证
      * @param password {string} 密码
-     * @return {Promise<Response>}
+     * @return {Promise<any>}
      */
-    this.adminLogin = password => fetch(`${api}admin-certificate/`, {
-        method: 'POST',
-        body: password
-    })
+    this.adminLogin = password => fetchHelper('POST', 'admin-certificate', password)
     /**
      * 用户身份认证。（每 60s 内只可调用一次）
      *
      * 调用接口后会向用户发送包含验证码的邮件，将用户收到的验证码作为参数调用接口返回的函数即可判断是否认证成功。
      *
-     * @return {Promise<function(string):Promise<void>>}
+     * @return {Promise<function(string):Promise<any>>}
      */
     this.userLogin = () => {
-        const url = `${api}auth-certificate/`
-        return fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({email: emailGetter()})
-        }).then(response => response.json)
-            .then(json => {
-                if (json.status !== 200) throw json
-                return (code) => fetch(url, {
-                    method: 'POST',
-                    body: JSON.stringify({code})
-                }).then(response => response.json())
-                    .then(json => {
-                        if (json.status !== 200) throw json
-                    })
-            })
+        const type = 'auth-certificate'
+        return fetchHelper('POST', type, {email: emailGetter()})
+            .then(() => (
+                (code) => fetchHelper('POST', type, {code})
+            ))
     }
     /**
      * 取消用户身份认证
-     * @return {Promise<Response>}
+     * @return {Promise<any>}
      */
-    this.logout = () => fetch(`${api}log-out/`, {method: 'POST'})
+    this.logout = () => fetchHelper('POST', 'log-out')
     /**
      * 获取指定页面的访问量统计
      *
@@ -118,19 +128,12 @@ function Kments(api, nameGetter, emailGetter, linkGetter) {
      */
     this.count = (pathname) => {
         if (Array.isArray(pathname)) {
-            return fetch(`${api}get-pages-view/`, {
-                method: 'POST',
-                body: JSON.stringify(pathname)
-            })
+            return fetchHelper('POST', 'get-pages-view', pathname)
+                .then(json => json.data)
         } else {
-            const body = pathname ? {body: pathname} : {}
-            return fetch(`${api}count-visit/`, Object.assign(
-                {method: 'POST'}, body
-            )).then(response => response.json())
-                .then(json => {
-                    if (json.start !== 200) throw json
-                    return [json.data]
-                })
+            const body = pathname ? {body: pathname} : null
+            return fetchHelper('POST', 'count-visit', body)
+                .then(json => [json.data])
         }
     }
     /**
@@ -147,20 +150,16 @@ function Kments(api, nameGetter, emailGetter, linkGetter) {
      * ```
      *
      * @param data {any} 每个元素的 key 为 `pathname`，value 为要删除的评论的 ID 数组
-     * @return {Promise<Response>}
+     * @return {Promise<any>}
      */
-    this.delete = (data) => fetch(`${api}delete-comments/`, {
-        method: 'POST',
-        body: JSON.stringify(data)
-    })
+    this.delete = (data) => fetchHelper('DELETE', 'delete-comments', data)
     /**
      * 隐藏指定评论，管理员与已认证用户均可使用
      * @param values {string[]} 要隐藏的评论的 ID
      * @param page {string} 评论所在页面的 pathname
-     * @return {Promise<Response>}
+     * @return {Promise<number>} 隐藏失败的数量
      */
-    this.hide = (values, page = location.pathname) => fetch(`${api}hide-comments/`, {
-        method: 'POST',
-        body: JSON.stringify({page, values})
-    })
+    this.hide = (values, page = location.pathname) =>
+        fetchHelper('PUT', 'hide-comments', {page, values})
+            .then(json => json.fails)
 }
