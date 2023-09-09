39aab7221ed1dcdf13cb44906aacbc7d8d199b15
kmar <minedreams@qq.com>
2023-09-09 18:54:14 +0800

    修改管理员 ID 的 cookie 名称

--- src/ts/api/AdminCertificate.ts
+++ src/ts/api/AdminCertificate.ts
@@ -2,6 +2,8 @@ import {KmentsPlatform} from '../KmentsPlatform'
 import {connectRedis} from '../RedisOperator'
 import {calcHash, initRequest, isDev} from '../utils'
 
+const cookieKey = 'kments-admin-id'
+
 // noinspection JSUnusedGlobalSymbols
 /**
  * 管理员身份认证
@@ -17,14 +19,14 @@ export async function certifyAdmin(platform: KmentsPlatform) {
         return platform.sendJson(403, {msg: '密码错误'})
     const adminId = calcHash('md5', config.encrypt(`${Date.now()}-${password}-${Math.random()}`))
     const domain = isDev ? 'localhost' : config.admin.domUrl.host
-    platform.setCookie(`admin=${adminId}; Max-Age=2592000; Domain=${domain}; Path=/; Secure; HttpOnly; SameSite=None;`)
+    platform.setCookie(`${cookieKey}=${adminId}; Max-Age=2592000; Domain=${domain}; Path=/; Secure; HttpOnly; SameSite=None;`)
     await connectRedis().setex('admin', 2592000, adminId)
     platform.sendJson(200)
 }
 
 /** 校验管理员身份 */
 export async function verifyAdminStatus(platform: KmentsPlatform): Promise<boolean> {
-    const value = platform.readCookie('admin')
+    const value = platform.readCookie(cookieKey)
     if (!value) return false
     const realId = await connectRedis().get('admin')
     return realId == value
